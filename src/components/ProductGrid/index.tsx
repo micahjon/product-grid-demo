ce55d50d3904b9111613761eccce6cd590e7c2b7
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2021-10-09 12:21:21 -0700

    Add ProductGridItem component
    Can be added/removed from cart

--- src/components/ProductGrid/index.tsx
+++ src/components/ProductGrid/index.tsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
 import Chip from '../Chip'
+import ProductGridItem from '../ProductGridItem'
 import './style.css'
 
+type Product = {
+  productId: { value: string },
+  name: string,
+  imageUrl: string,
+  price: number,
+  category: string
+}
+
+
+
 function ProductGrid() {
+  // Categories
   const categories = ['Alcohol', 'Bakery']
   const [selectedCategories, setSelectedCategories] = useState([] as string[])
+
+  // Cart (map of product IDs to quantity)
+  const [cart, updateCart] = useState({} as {[key: string]: number});
+
+  // Products (@todo -> fetch from API)
+  const [products, setProducts] = useState([
+    {
+      "productId": {
+        "value": "0d101385-3cdc-464b-8431-ec0b50618b8a"
+      },
+      "name": "Sir Kensington’s Organic Mayonnaise, 12 oz",
+      "imageUrl": "https://www.sirkensingtons.com/uploads/products/_product2x/SK-20-Render-Mayonnaise-Organic-12oz-Jar-FRONT-V02.png",
+      "category": 'Alcohol',
+      "price": 749,
+    }
+  ] as Product[]);
+
+  const maxProducts = 12;
+  const productsToDisplay = selectedCategories.length 
+    ? products.filter(p => selectedCategories.includes(p.category)).slice(0, maxProducts)
+    : products.slice(0, maxProducts);
 
   return (
     <section className="product-grid">
@@ -12,17 +45,33 @@ function ProductGrid() {
       {/* Product filters (maybe move to separate component) */}
       <h2 className="product-grid__filter-heading">Shop by Category</h2>
       <div className="product-grid__filter-choices">
-        {categories.map(name => {
-          const isSelected = selectedCategories.includes(name);
-          const onSelect = () => setSelectedCategories([...selectedCategories, name]);
-          const onDeselect = () => setSelectedCategories(selectedCategories.filter(n => n !== name));
-          return <Chip key={name} name={name} isSelected={isSelected} onDeselect={onDeselect} onSelect={onSelect} className="product-grid__filter-choice" />
-        })}
+        {categories.map(name => 
+          <Chip 
+            key={name} 
+            name={name} 
+            isSelected={selectedCategories.includes(name)} 
+            onDeselect={() => setSelectedCategories(selectedCategories.filter(n => n !== name))} 
+            onSelect={() => setSelectedCategories([...selectedCategories, name])} 
+            className="product-grid__filter-choice" 
+          />
+        )}
       </div>
 
       {/* Product grid */}
       <div className="product-grid__items">
-        {/* @todo -> add products */}
+        {productsToDisplay.map((product) => 
+          <ProductGridItem 
+            quantityInCart={cart[product.productId.value] || 0} 
+            onAddToCart={() => {
+              const id = product.productId.value;
+              updateCart({
+                ...cart,
+                [id]: cart[id] ? cart[id] + 1 : 1,
+              })
+            }}
+            {...product} 
+          />
+        )}
       </div>
 
     </section>
---
+++ src/components/ProductGridItem/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './style.css'
+
+type ProductGridItemProps = {
+  name: string,
+  imageUrl: string, // @todo -> add multiple sources (srcset)
+  price: number,
+  quantityInCart: number,
+  onAddToCart: (event: any) => void,
+}
+
+function ProductGridItem({name, imageUrl, price, quantityInCart, onAddToCart}: ProductGridItemProps) {
+  return <article className={`product ${quantityInCart > 0 ? 'product--in-cart' : ''}`}>
+    {quantityInCart > 0 ? <span className="product__quantity-in-cart">{quantityInCart}</span> : ''}
+    <figure className="product__image-container" style={{backgroundImage: `url('${imageUrl}')`}}></figure>
+    <h1 className="product__name">{name}</h1>
+    <p className="product__price">{formatPrice(price, 'en-US')}</p>
+    <button className="product__add-to-cart" onClick={onAddToCart}>Add to Cart</button>
+  </article>
+}
+
+// Format price in USD
+// Can easily be extended to support any currency & language
+function formatPrice(usdCents: number, locale: string) {
+  const numberFormat = new Intl.NumberFormat([locale], {
+      style: 'currency',
+      currency: 'usd',
+      currencyDisplay: 'symbol',
+  });
+  return numberFormat.format(usdCents / 100);
+}
+
+export default ProductGridItem
