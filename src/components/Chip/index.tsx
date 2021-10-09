d80d120545b083e07a4f1e73c67f2bb95c83bf0d
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2021-10-09 11:43:43 -0700

    Add Chip component for filtering by category
    Chips can be selected or deselected

---
+++ src/components/Chip/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './style.css'
+
+type ChipProps = {
+  name: string,
+  isSelected: boolean, 
+  onSelect: (event: any) => void, 
+  onDeselect: (event: any) => void, 
+  className?: string,
+};
+
+function Chip({ name, isSelected, onSelect, onDeselect, className }: ChipProps) {
+  const classNames = [
+    'chip',
+    isSelected ? 'chip--selected' : 'chip--deselected',
+    className,
+  ].filter(Boolean).join(' ');
+
+  if (!isSelected) {
+    // Click button to select
+    return <button className={classNames} onClick={onSelect}>{name}</button>
+  }
+
+  // Click close (X) button to deselect
+  return <div className={classNames}>
+    {name}
+    <button className="chip__x" onClick={onDeselect}>X</button>
+  </div>
+}
+
+export default Chip
--- src/components/ProductGrid/index.tsx
+++ src/components/ProductGrid/index.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import Chip from '../Chip'
 import './style.css'
 
 function ProductGrid() {
+  const categories = ['Alcohol', 'Bakery']
+  const [selectedCategories, setSelectedCategories] = useState([] as string[])
+
   return (
     <section className="product-grid">
 
       {/* Product filters (maybe move to separate component) */}
       <h2 className="product-grid__filter-heading">Shop by Category</h2>
-      {/* @todo -> add choices */}
+      <div className="product-grid__filter-choices">
+        {categories.map(name => {
+          const isSelected = selectedCategories.includes(name);
+          const onSelect = () => setSelectedCategories([...selectedCategories, name]);
+          const onDeselect = () => setSelectedCategories(selectedCategories.filter(n => n !== name));
+          return <Chip key={name} name={name} isSelected={isSelected} onDeselect={onDeselect} onSelect={onSelect} className="product-grid__filter-choice" />
+        })}
+      </div>
 
       {/* Product grid */}
       <div className="product-grid__items">
